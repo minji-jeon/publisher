fe8dcc7b969f5d4e3a9e5cbc657180ea45ad70f5
minji-Jeon <088@mediabrain.co.kr>
2021-04-22 10:52:38 +0900

    브랜치 테스트

--- babel-test/ES5/index.js
+++ babel-test/ES5/index.js
@@ -11,3 +11,22 @@ var v = 1; //https://www.daleseo.com/js-babel/ 참고
 //.babelrc 파일생성 { "presets": ["@babel/env"] } 붙여넣기
 //package.json test에 "babel ./ES6(작성폴더) -d ./ES5(출력폴더) -w"
 //디버그 클릭
+
+var namez = 'min';
+var objmin = {
+  age: 29,
+  namez: namez,
+  getName: function getName() {
+    return this.name;
+  }
+}; // console.log(objmin);
+
+var name2 = 'minn';
+var age = 29;
+console.log({
+  namez: namez,
+  age: age
+}); //{name: "min", age: 29} 단축속성명을 사용하여 변수의 이름과 값을 한번에 확인이 가능하다.
+
+var input = '오늘 보실 드라마는 최근 시작한 드라마입니다.';
+console.log(input.replace(/드라마/g, '영화'));
--- babel-test/ES6/index.js
+++ babel-test/ES6/index.js
@@ -11,3 +11,21 @@ const v = 1;
 //.babelrc 파일생성 { "presets": ["@babel/env"] } 붙여넣기
 //package.json test에 "babel ./ES6(작성폴더) -d ./ES5(출력폴더) -w"
 //디버그 클릭
+const namez = 'min';
+const objmin = {
+  age : 29,
+  namez,
+  getName() {
+    return this.name;
+  },
+};
+// console.log(objmin);
+
+const name2 = 'minn';
+const age = 29;
+
+console.log({namez,age}); //{name: "min", age: 29} 단축속성명을 사용하여 변수의 이름과 값을 한번에 확인이 가능하다.
+
+
+const input = '오늘 보실 드라마는 최신 시작한 드라마입니다.';
+console.log(input.replaceAll('드라마', '영화'));
