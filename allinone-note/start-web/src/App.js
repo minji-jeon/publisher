e06f89531afaa48a90f551c0abf28515489d9544
minji-Jeon <088@mediabrain.co.kr>
2021-07-28 16:40:08 +0900

    개발클래스 - 업로드 & 제품 상세 페이지

--- allinone-note/start-web/src/App.js
+++ allinone-note/start-web/src/App.js
@@ -1,13 +1,53 @@
 import './App.css';
 import MainPageComponent from "./main/index.js"
-import {Switch, Route} from "react-router-dom";
+import {Switch, Route, Link, useHistory} from "react-router-dom";
 import UploadPage from "./upload";
 import ProductPage from "./product";
 
 
 function App() {
+  var history=useHistory();
   return (
     <div>
+      <a href="#main" id="skip-nav">본문 바로가기</a>
+      <header>
+        <article className="top_header">
+          <div className="inner">
+            <h1 className="logo">
+              <Link to="/">
+                <img src="/img/main/header_logo.jpg" alt="파스쿠찌 로고"/>
+              </Link>
+            </h1>
+            <div className="sns_list clear">
+              <button className="new-button" type="button" onClick={function() {
+                history.push("/upload")
+              }}>
+                상품 업로드
+              </button>
+            </div>
+            <form className="H_Search">
+              <label for="Search">통합검색</label>
+              <div>
+                <input type="search" id="Search" />
+                <a href="#"><i className="fas fa-search"></i></a>
+              </div>
+            </form>
+          </div>
+        </article>
+        <article className="gnb">
+          <div className="inner">
+            <button><i className="fas fa-bars"></i></button>
+            <nav>
+              <a href="sub01.html">CAFFE PASCUCCI</a>
+              <a href="sub02.html">MENU</a>
+              <a href="sub03.html">EVENT&amp;TALK</a>
+              <a href="sub04.html">STORE</a>
+              <a href="sub05.html">CUSTOMER</a>
+              <a className="red" href="sub06.html">FRANCHISH</a>
+            </nav>
+          </div>
+        </article>
+      </header>
       <Switch>
         <Route exact={true} path="/">
           <MainPageComponent/>
@@ -19,6 +59,27 @@ function App() {
           <UploadPage></UploadPage>
         </Route>
       </Switch>
+      <footer id="footer">
+        <div className="inner clear">
+          <div className="logo"><img src="img/main/foot_logo.jpg" alt="" /></div>
+          <div className="foot_content">
+            <aside className="foot_link clear">
+              <a href="#">해피포인트</a>
+              <a className="red" href="#">개인정보 처리방침</a>
+              <a href="#">영상정보처리기기운영 &#46; 관리방침</a>
+              <a href="#">이용약관</a>
+              <a href="#">윤리신고센터</a>
+              <a href="#">거래희망사전등록</a>
+            </aside>
+            <p>
+              서울시 서초구 양재동 <span>11-149</span>번지
+              <span>FAX.0505-076-0210</span> 고객상담센터
+              <span>080-731-2027</span>
+            </p>
+            <p>COPYRIGHT 2019 THE CAFFEE PASCUCCI</p>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
--- allinone-note/start-web/src/main/index.js
+++ allinone-note/start-web/src/main/index.js
@@ -22,89 +22,69 @@ function MainPage() {
 
   return (
     <div>
-          <section id="visual">
-      <div className="inner">
-        <div className="visual_text">
-          <p className="R_49">Pascucci menu</p>
-          <p className="N_20">130년 정통 이탈리아 에스프레소 커피전문점</p>
+      <section id="visual">
+        <div className="inner">
+          <div className="visual_text">
+            <p className="R_49">Pascucci menu</p>
+            <p className="N_20">130년 정통 이탈리아 에스프레소 커피전문점</p>
+          </div>
         </div>
-      </div>
-    </section>
-    {/* visual end */}
+      </section>
+      {/* visual end */}
 
-    <main id="main">
-      {/* scene02 start */}
-      <ul className="sub_menu clear">
-        <li><a className="on" href="#">HOT</a></li>
-        <li><a href="#">ICED</a></li>
-        <li><a href="#">BEAN</a></li>
-      </ul>
-    </main>
-    {/* scene02 end */}
+      <main id="main">
+        {/* scene02 start */}
+        <ul className="sub_menu clear">
+          <li><a className="on" href="#">HOT</a></li>
+          <li><a href="#">ICED</a></li>
+          <li><a href="#">BEAN</a></li>
+        </ul>
+      </main>
+      {/* scene02 end */}
 
-    <section id="scene03">
-      {/* scene03 start */}
-      <div className="inner_s clear">
-        <h2 className="N_17">따뜻한 커피</h2>
-        <p>풍미 깊은 맛을 따뜻하게 만나는 커피</p>
-      </div>
-    </section>
-    {/* scene03 end */}
-
-    <section id="scene04">
-      {/* scene04 start */}
-      <ul className="inner_s prodduct-card">
-        {
-          products.map(function(product, index) {
-            return (
-              <li>
-                <Link to={'/product/'+index}>
-                  <img src={product.imgUrl} alt="" />
-                  <p className="title">{product.name}</p>
-                  <p className="sub_text">
-                    {product.description}
-                  </p>
-                  <p><span>{product.tag}</span></p>
-                </Link>
-              </li>
-            )
-          })
-        }
-        {/* <li>
-          <a href="#">
-            <img src="img/manu/menu_img18.jpg" alt="" />
-            <p className="title">제주 말차 샷 라떼</p>
-            <p className="sub_text">
-              100% 제주 녹차 파우더로 쌉싸름한 맛과<br />에스프레소의 향이
-              더해진 부드러운 라떼
-            </p>
-            <p><span>&#35; 녹차 &#35; 원두</span></p></a>
-        </li> */}
-      </ul>
-    </section>
-    {/* scene04 end */}
-
-    <footer id="footer">
-      <div className="inner clear">
-        <div className="logo"><img src="img/main/foot_logo.jpg" alt="" /></div>
-        <div className="foot_content">
-          <aside className="foot_link clear">
-            <a href="#">해피포인트</a>
-            <a className="red" href="#">개인정보 처리방침</a>
-            <a href="#">영상정보처리기기운영 &#46; 관리방침</a>
-            <a href="#">이용약관</a>
-            <a href="#">윤리신고센터</a>
-            <a href="#">거래희망사전등록</a>
-          </aside>
-          <p>
-            서울시 서초구 양재동 <span>11-149</span>번지
-            <span>FAX.0505-076-0210</span> 고객상담센터
-            <span>080-731-2027</span>
-          </p>
-          <adrress>COPYRIGHT 2019 THE CAFFEE PASCUCCI</adrress>
+      <section id="scene03">
+        {/* scene03 start */}
+        <div className="inner_s clear">
+          <h2 className="N_17">따뜻한 커피</h2>
+          <p>풍미 깊은 맛을 따뜻하게 만나는 커피</p>
         </div>
-      </div>
-    </footer>
+      </section>
+      {/* scene03 end */}
+
+      <section id="scene04">
+        {/* scene04 start */}
+        <ul className="inner_s prodduct-card">
+          {
+            products.map(function(product, index) {
+              return (
+                <li>
+                  <Link to={'/product/'+product.id}>
+                    <img src={product.imgUrl} alt="" />
+                    <p className="title">{product.name}</p>
+                    <p className="sub_text">
+                      {product.description}
+                    </p>
+                    <p><span>{product.tag}</span></p>
+                  </Link>
+                </li>
+              )
+            })
+          }
+          {/* <li>
+            <a href="#">
+              <img src="img/manu/menu_img18.jpg" alt="" />
+              <p className="title">제주 말차 샷 라떼</p>
+              <p className="sub_text">
+                100% 제주 녹차 파우더로 쌉싸름한 맛과<br />에스프레소의 향이
+                더해진 부드러운 라떼
+              </p>
+              <p><span>&#35; 녹차 &#35; 원두</span></p></a>
+          </li> */}
+        </ul>
+      </section>
+      {/* scene04 end */}
+
+    
     </div>
   );
 }
